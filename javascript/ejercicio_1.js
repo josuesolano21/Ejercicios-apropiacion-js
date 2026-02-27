33ae6a42209236fa003681af5d367ab18f84df30
Julian Diaz <juliandiazotavo@gmail.com>
2026-02-27 17:35:56 -0500

    realize los ejericios 1,2 y 3

--- javascript/ejercicio_1.js
+++ javascript/ejercicio_1.js
@@ -1,3 +1,6 @@
 //* 1. Saludo personalizado
 // Solicita al usuario su nombre y edad.
 // Muestra en consola: "Hola [nombre], tienes [edad] años." */
+let nombre = prompt("Cual es tu nombre")
+let edad = prompt("Cuantos años tienes")
+console.log("Hola", + nombre + "Tienes" + edad + "años") 
--- javascript/ejercicio_2.js
+++ javascript/ejercicio_2.js
@@ -1,3 +1,7 @@
 /* 2. Área de un rectángulo
 • Solicita base y altura.
 • Calcula y muestra el área. */
+let base = prompt("Ingrese la base del rectangulo")
+let altura = prompt("Ingrese la altura del rectangulo")
+let area = base * altura;
+console.log(`El area del rectangulo es: ${area}`);
