adf36e031af9e3a3cb8216823469d419913d9302
josuesolano21 <josues230@hotmail.com>
2026-02-27 17:04:37 -0500

    Ejercicio 7 js resuelto

--- javascript/ejercicio_7.js
+++ javascript/ejercicio_7.js
@@ -1,3 +1,15 @@
 /* 7. Calcular el doble y el triple
 • Solicita un número.
 • Muestra su doble y su triple. */
+
+
+// Solicitar número al usuario
+let numero = Number(prompt("Ingresa un número:"));
+
+// Calcular doble y triple
+let doble = numero * 2;
+let triple = numero * 3;
+
+// Mostrar resultado
+alert("El doble es: " + doble);
+alert("El triple es: " + triple);
